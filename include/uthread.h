444acbaad74684739622ce3fa8e84f4cfe5cf8ef
Kapil1331 <kapiltangsali@gmail.com>
2025-03-15 23:22:17 +0530

    add function arguments

--- include/uthread.h
+++ include/uthread.h
@@ -1,14 +1,13 @@
-struct uthread {
+typedef struct uthread {
 	int tid;
 	void *stack;
 	void *(*func)(void *);
 	void *arg;
 	struct uthread* next;
-}
+} uthread;
 
-int uthread_create();
-int uthread_join();
+int uthread_create(uthread *thread, void *(*func)(void *), void *arg);
+int uthread_join(uthread thread);
 void uthread_exit();
-int uthread_cancel();
-int uthread_kill();
-
+int uthread_cancel(uthread thread);
+int uthread_kill(uthread thread);
