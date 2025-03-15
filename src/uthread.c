46d4ab67ac33ece65784ffa880d5ec3711dade6c
Kapil1331 <kapiltangsali@gmail.com>
2025-03-16 00:42:16 +0530

    add uthread_create()

--- src/uthread.c
+++ src/uthread.c
@@ -1,1 +1,18 @@
 #include "uthread.h"
+
+int uthread_create(uthread *thread, void *(*func)(void *), void *arg){
+
+	thread->stack = malloc(1024);
+
+	thread->func = func;
+	thread->arg = arg;
+
+	pid_t tid = clone((int (*)(void *)) thread->func, thread->stack + 1024, SIGCHLD, thread->arg);
+	if(tid == -1){
+		free(stack);
+		return -1;
+	}
+
+	thread->tid  = (int)tid;
+	return 0;
+}
